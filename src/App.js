77224214a445ce210d4be671c9ddfb2a62ba502d
Tadesse Jemal <tadessejemal63@gmail.com>
2022-06-02 10:37:49 +0300

    portfolio-eddited 2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './portfolioComponents/Navbar/Navbar';
 import Home from './portfolioComponents/Resume/Home';
 import Testmonial from './portfolioComponents/Testmonials/Testmonial';
 import About from './portfolioComponents/AboutMe/About';
-import Certification from './portfolioComponents/Abbb/Certification';
+import Certification from './portfolioComponents/Certification/Certification';
 import Portfolios from './portfolioComponents/Portfolios/Portfolios';
 import Contacts from './portfolioComponents/Contact/Contacts';
 import Services from './portfolioComponents/Services/Services';
