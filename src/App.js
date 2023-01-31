22d752f31d140bf6fd5b789306d5bec336a8d671
Tadesse Jemal <tadessejemal63@gmail.com>
2023-01-31 15:48:12 +0300

    update app page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from './portfolioComponents/Navbar/Navbar';
 import Home from './portfolioComponents/Home/Home';
 import Testmonial from './portfolioComponents/Testmonials/Testmonial';
 import About from './portfolioComponents/About/About';
-import Certification from './portfolioComponents/Certification/Certification';
 import Portfolios from './portfolioComponents/Portfolios/Portfolios';
 import Contacts from './portfolioComponents/Contact/Contacts';
 import Services from './portfolioComponents/Services/Services';
@@ -15,7 +14,6 @@ function App() {
               <Navbar />
               <Home />
               <About />
-              <Certification />
               <Services />
               <Portfolios />
               <Testmonial />
